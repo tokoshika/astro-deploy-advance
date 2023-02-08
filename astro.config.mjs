66dce48c71669f3873c5b154873914f960bfa4a9
pythonthm <pythonthm@gmail.com>
2023-02-08 13:51:59 +0900

    advance

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://tokoshika.github.io',
-    base: '/astro-deploy',
+    base: '/astro-deploy-advance',
 });
